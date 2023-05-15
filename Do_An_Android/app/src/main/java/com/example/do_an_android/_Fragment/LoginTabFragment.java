cdf184ae21aaee208e14d1dd59a15bbbb34b60d1
vidao1233 <114890306+vidao1233@users.noreply.github.com>
2023-05-16 01:14:08 +0700

    Update LoginTabFragment.java
    add message when login fail

--- Do_An_Android/app/src/main/java/com/example/do_an_android/_Fragment/LoginTabFragment.java
+++ Do_An_Android/app/src/main/java/com/example/do_an_android/_Fragment/LoginTabFragment.java
@@ -140,7 +140,7 @@ public class LoginTabFragment extends Fragment implements View.OnClickListener {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Toast.makeText(context, "Đã xảy ra lỗi!", Toast.LENGTH_SHORT).show();
             }
         }) {
             @Nullable
